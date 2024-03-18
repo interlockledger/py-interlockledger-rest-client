99d357ada79b0c820bb4210437a7a5bc55360258
Daniel Chino <dchino@opencs.com.br>
2024-03-18 18:56:59 -0300

    Add client basic test. (#1)

---
+++ tests/test_client.py
@@ -0,0 +1,23 @@
+import os
+from unittest import TestCase
+from src.pyil2.client import IL2Client
+
+class IL2ClientTest(TestCase):
+    def setUp(self) -> None:
+        self.filepath = os.environ.get('TEST_CERTIFICATE_PATH')
+        self.password = os.environ.get('TEST_CERTIFICATE_PASS')
+        self.host = os.environ.get('TEST_HOST')
+
+        if not self.filepath or not self.password or not self.host:
+            self.skipTest('Skipping TEST_CERTIFICATE_PATH or TEST_CERTIFICATE_PASS or TEST_HOST are not set as environment variables.')
+    
+    def test_request_with_certificate(self):
+        client = IL2Client(
+            host=self.host,
+            cert_filepath=self.filepath,
+            cert_password=self.password
+        )
+        resp = client._request('/', 'GET')
+        self.assertEqual(resp.status_code, 200)
+        
+    
