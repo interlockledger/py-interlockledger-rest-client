60998fd60dfb46a4d6a6ad1798fc9233f8f84102
Daniel Chino <dchino@opencs.com.br>
2024-03-18 18:47:51 -0300

    Add base request client. (#1)

---
+++ src/pyil2/client.py
@@ -0,0 +1,196 @@
+import contextlib
+import tempfile
+from typing import Any, Dict, List, Union
+import requests
+import urllib.parse
+from base64 import b64encode
+from .utils.certificates import PKCS12Certificate
+
+class IL2Client:
+    '''
+     REST API client to the InterlockLedger node.
+
+    You'll try to establish a bi-authenticated https connection with the 
+    configured node API address and port. The client-side certificate used 
+    to connect needs to be configured with the proper layered authorization 
+    role in the node configuration file and imported into a key permitted to 
+    update the chain that will be used. 
+
+    Args:
+        host (`str`): Host address in the format: scheme://hostmane[:port][/].
+        cert_filepath (:obj:`str`): Path to the .pfx certificate. Please refer to the InterlockLedger manual to see how to create and import the certificate into the node.
+        cert_password (:obj:`str`): Password of the .pfx certificate.
+        verify_ca (`bool`): If `True`, verifies the SSL certificate in a CA (default: True).
+        connect_timeout (:obj:`int`): Connect timeout in seconds (default: 5s).
+        read_timeout (:obj:`int`): Read timeout in seconds (default 30s).
+    '''
+    
+    _available_apis = []
+    
+    def __init__(self, 
+            host: str,
+            cert_filepath: str,
+            cert_password: str,
+            verify_ca: bool=True,
+            timeout:int = 30,
+        ):
+        self.host = host
+        if self.host[-1] != '/':
+            self.host += '/'
+        self.verify_ca = verify_ca
+        self.timeout = timeout
+        self._session = None
+        self._pem_file = None
+        self.certificate = PKCS12Certificate(cert_filepath, cert_password)
+        
+    @property
+    def public_certificate_in_x509(self):
+        """:obj:`str`: Public certificate in X509 format."""
+        x509 = (
+            self.certificate.public_certificate
+                .replace(b'-----BEGIN CERTIFICATE-----\n', b'')
+                .replace(b'-----END CERTIFICATE-----\n', b'')
+                .replace(b'\n', b'')
+        )
+        return x509
+    
+    @property
+    def api_list(self) -> List[str]:
+        '''
+        [:obj:`str`]: List of available APIs.
+        '''
+        return self._available_apis
+
+    def api(self, name: str) -> None:
+        '''
+        Get an instance of an API.
+
+        Args:
+            name (:obj:`str`): API name.
+        
+        Returns:
+            `BaseApi`: Instance of an API.
+        '''
+        name = name.lower()
+        match name:
+            case _:
+                raise ValueError(f'No API with name {name} found. Must be in {self._available_apis}')
+    
+    def _get_session(self) -> requests.Session:
+        if not self._session:
+            self._pfx_to_pem()
+            self._session = requests.Session()
+            self._session.cert = self._pem_file.name
+            self._session.verify = self.verify_ca
+        return self._session
+
+    @contextlib.contextmanager
+    def _pfx_to_pem(self) -> None:
+        self._pem_file = tempfile.NamedTemporaryFile(suffix='.pem')
+        f_pem = open(self._pem_file.name, 'wb')
+        f_pem.write(self.certificate.private_key)
+        f_pem.write(self.certificate.public_certificate)
+        f_pem.close()
+
+    def __del__(self):
+        if self._pem_file:
+            self._pem_file.close()
+        if self._session:
+            self._session.close()
+
+    def _join_uri(self, url: str):
+        return urllib.parse.urljoin(self.host, url)
+
+    def _prepare_headers(self, accept: str, content_type: str=None, auth: bool=True):
+        headers = {
+            'Accept':accept
+        }
+        if content_type:
+            headers['Content-type'] = content_type
+        return headers
+
+    def _handle_error_response(self, response: requests.Response) -> requests.Response:
+        return response
+
+    def _request(self, 
+            url: str,
+            method: str,
+            accept: str='application/json',
+            content_type: str='application/json',
+            body: Dict[str, Any]=None, 
+            params: Dict[str, str]={},
+            auth: bool=True
+        ) -> requests.Response:
+        method = method.upper()
+
+        match method:
+            case 'GET':
+                resp = self._get(url=url, accept=accept, params=params, auth=auth)
+            case 'DELETE':
+                resp = self._delete(url=url, accept=accept, params=params, auth=auth)
+            case 'POST':
+                resp = self._post(url=url, accept=accept, content_type=content_type, body=body, auth=auth)
+            case 'PATCH':
+                resp = self._patch(url=url, accept=accept, content_type=content_type, body=body, auth=auth)
+            case 'PUT':
+                resp = self._put(url=url, accept=accept, content_type=content_type, body=body, auth=auth)
+        
+        return self._handle_error_response(resp)
+    
+    def _get(self, url: str, accept: str, params: Dict[str, str], auth: bool=True):
+        cur_uri = self._join_uri(url)
+        headers = self._prepare_headers(accept, auth=auth)
+        response = self._get_session().get(
+            cur_uri,
+            headers=headers,
+            params=params,
+            timeout=self.timeout,
+        )
+        return response
+
+    def _delete(self, url: str, accept: str, params: Dict[str, str], auth: bool=True):
+        cur_uri = self._join_uri(url)
+        headers = self._prepare_headers(accept, auth=auth)
+        response = self._get_session().delete(
+            cur_uri,
+            headers=headers,
+            params=params,
+            timeout=self.timeout,
+        )
+        return response
+
+    def _patch(self, url: str, accept: str, content_type: str, body: Dict[str, Any], auth: bool=True):
+        cur_uri = self._join_uri(url)
+        headers = self._prepare_headers(accept, content_type, auth=auth)
+        response = self._get_session().patch(
+            url=cur_uri,
+            headers=headers,
+            json=body,
+            timeout=self.timeout,
+        )
+        return response
+    
+    def _put(self, url: str, accept: str, content_type: str, body: Dict[str, Any], auth: bool=True):
+        cur_uri = self._join_uri(url)
+        headers = self._prepare_headers(accept, content_type, auth=auth)
+        response = self._get_session().put(
+            url=cur_uri,
+            headers=headers,
+            json=body,
+            timeout=self.timeout,
+        )
+        return response
+
+    def _post(self, url: str, accept: str, content_type: str, body: Dict[str, Any], auth: bool=True):
+        cur_uri = self._join_uri(url)
+        headers = self._prepare_headers(accept, content_type, auth=auth)
+        response = self._get_session().post(
+            url=cur_uri,
+            headers=headers,
+            json=body,
+            timeout=self.timeout,
+        )
+        return response
+
+
+
