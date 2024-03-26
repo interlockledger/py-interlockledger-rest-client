032a7d385958141a37c998d8f7175cb2a59f1df6
Daniel Chino <dchino@opencs.com.br>
2024-03-26 15:31:06 -0300

    Add create chain request. (#6)

--- src/pyil2/api/chain.py
+++ src/pyil2/api/chain.py
@@ -22,3 +22,26 @@ class ChainApi(BaseApi):
         if resp.status_code != 200:
             raise Exception
         return chain_models.ChainIdModel.validate_list_python(resp.json())
+
+    def create_chain(self, new_chain: chain_models.ChainCreationModel) -> chain_models.ChainCreatedModel:
+        """
+        Create a new chain.
+
+        Args:
+            model (:obj:`models.ChainCreationModel`): Model with the new chain attrbutes.
+
+        Returns:
+            :obj:`models.ChainCreatedModel`: Chain created model.
+        """
+        if not isinstance(new_chain, chain_models.ChainCreationModel):
+            raise ValueError("'new_chain' must be a ChainCreationModel.")
+        
+        resp = self._client._request(
+            url=f'{self.base_url}',
+            method='post',
+            body=new_chain.model_dump(exclude_none=True, by_alias=True)
+        )
+        if resp.status_code != 201:
+            raise Exception
+        return chain_models.ChainCreatedModel(**resp.json())
+        pass
