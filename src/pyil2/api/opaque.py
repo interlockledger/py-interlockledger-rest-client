b7568230c581822888ff8954ed5bef4974e81440
Daniel Chino <dchino@opencs.com.br>
2024-04-06 22:37:30 -0300

    Add query opaque records. (#14)

--- src/pyil2/api/opaque.py
+++ src/pyil2/api/opaque.py
@@ -1,5 +1,7 @@
+from typing import List
 from .base import BaseApi
 from ..models.errors import ErrorDetailsModel
+from ..models.base import ListModel
 from ..models.record import OpaqueRecordModel
 
 class OpaqueApi(BaseApi):
@@ -83,3 +85,47 @@ class OpaqueApi(BaseApi):
             payload=resp.content,
         )
         return model
+
+    def query_opaque(self,
+            chain_id: str,
+            application_id: int,
+            payload_type_ids: List[int]=[],
+            how_many: int=None,
+            last_to_first: bool=False,
+            page: int=0,
+            size: int=10,
+        ) -> ListModel[OpaqueRecordModel] | ErrorDetailsModel:
+        """
+        Query opaque records in a chain.
+
+        Args:
+            chain_id (`str`): Chain ID.
+            application_id (`int`): Application ID which records will be queried.
+            payload_type_ids ([`int`]): List of opaque payload type IDs.
+            how_many (`int`): How many records to return. If ommited or 0 returns all.
+            last_to_first (`bool`): If `True`, return the items in reverse order.
+            page (:obj:`int`): Page to return.
+            size (:obj:`int`): Number of items per page.
+
+        Returns:
+            :obj:`models.ListModel[models.records.OpaqueRecordModel]`: List of opaque records in a chain.
+        """
+        params = {
+            "appId": application_id,
+            "page": page,
+            "pageSize": size,
+            "lastToFirst": last_to_first,
+        }
+        if payload_type_ids:
+            params['payloadTypeIds'] = payload_type_ids
+        if how_many is not None:
+            params['howMany'] = how_many
+        
+        resp = self._client._request(
+            url=f'{self.base_url}{chain_id}/asJson/query',
+            method='get',
+            params=params,
+        )
+        if isinstance(resp, ErrorDetailsModel):
+            return resp
+        return ListModel[OpaqueRecordModel](**resp.json())
--- tests/api/test_opaque.py
+++ tests/api/test_opaque.py
@@ -1,4 +1,5 @@
 from .base import BaseApiTest
+from src.pyil2.models.base import ListModel
 from src.pyil2.models.errors import ErrorDetailsModel
 from src.pyil2.models.record import OpaqueRecordModel
 
@@ -37,3 +38,58 @@ class OpaqueApiTest(BaseApiTest):
     def test_get_opaque_invalid_serial(self):
         opaque = self.api.get_opaque(self.default_chain, 0)
         self.assertIsInstance(opaque, ErrorDetailsModel)
+    
+    def test_query_opaque(self):
+        opaques = self.api.query_opaque(
+            self.default_chain,
+            application_id=13
+        )
+        self.assertIsInstance(opaques, ListModel)
+        for item in opaques.items:
+            self.assertIsInstance(item, OpaqueRecordModel)
+    
+    def test_query_opaque_filter_tag_ids(self):
+        opaque = self.api.add_opaque(
+            chain_id=self.default_chain,
+            application_id=13,
+            payload_type_id=1313,
+            payload=b'test2'
+        )
+        opaque = self.api.add_opaque(
+            chain_id=self.default_chain,
+            application_id=13,
+            payload_type_id=131313,
+            payload=b'test2'
+        )
+
+        opaques = self.api.query_opaque(
+            self.default_chain,
+            application_id=13,
+            payload_type_ids=[1313],
+            last_to_first=True
+        )
+        self.assertIsInstance(opaques, ListModel)
+        for item in opaques.items:
+            self.assertIsInstance(item, OpaqueRecordModel)
+            self.assertEqual(item.payload_tag_id, 1313)
+    
+    def test_query_opaque_filter_empty_tag_ids(self):
+        opaques = self.api.query_opaque(
+            self.default_chain,
+            application_id=13,
+            payload_type_ids=[1414],
+            last_to_first=True
+        )
+        self.assertIsInstance(opaques, ListModel)
+        self.assertEqual(len(opaques.items), 0)
+            
+    def test_query_opaque_filter_empty_app_id(self):
+        opaques = self.api.query_opaque(
+            self.default_chain,
+            application_id=131313,
+        )
+        self.assertIsInstance(opaques, ListModel)
+        self.assertEqual(len(opaques.items), 0)
+            
+    
+    
